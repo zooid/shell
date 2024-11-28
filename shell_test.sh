a203c8e252533600c9b6a81a570478b0ae423f35
Ivelin Peychev <the.zooid@gmail.com>
2024-11-28 20:34:16 +0100

    Create shell_test.sh

---
+++ shell_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# shell_test.sh
+
+
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get upgrade -y
+
+sudo apt install wget -y
+sudo apt install curl -y
+
+#
+sudo apt install python3.11 -y
+sudo apt install pip -y
+pip install --upgrade pip
+
+sudo apt install python-venv -y
+
+
